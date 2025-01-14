48aa23c9d94bab070654ad96c33e4199d727d7d6
Ajay Gangwar <ajaygangwar24598@gmail.com>
2025-01-14 12:32:43 +0530

    added practice program

---
+++ lecture2/loops.py
@@ -0,0 +1,21 @@
+# loop on numbers
+# for i in [0, 1, 2, 3, 4, 5]:
+#     print(i)
+
+# loop on range of numbers
+# for i in range(6):
+#     print(i)
+
+# loop on range of numbers with start and end
+# for i in range(0, 6):
+#     print(i)
+
+# loop on string
+# name = "Harry"
+# for char in name:
+#     print(char)
+
+# loop on list
+names = ["Harry", "Ron", "Hermione"]
+for name in names:
+    print(name)
