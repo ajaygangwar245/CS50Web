d43d2b2897ffd7773e9b0090f928c8ec02fd8d6b
Ajay Gangwar <ajaygangwar24598@gmail.com>
2025-01-14 12:30:56 +0530

    added practice program

---
+++ lecture2/sequences.py
@@ -0,0 +1,34 @@
+name = "Harry"                                  # string
+
+print(name[0])
+print(type(name))
+print()
+
+names = ["Harry", "Ron", "Hermione"]            # list
+
+print(names)
+print(type(names))
+print()
+
+coordinate = (10.0, 20.0)                       # tuple
+
+print(coordinate)
+print(type(coordinate))
+print()
+
+ids = {"001", "002", "003"}                     # set
+
+print(ids)
+print(type(ids))
+print()
+
+
+phone = {
+    "brand": "IQOO",
+    "model": "Neo 9 Pro",
+    "year": 2024,
+    "processor": "SD 8 Gen 2"
+}                                              # dictionary
+
+print(phone)
+print(type(phone))
