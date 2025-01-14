8ee490aeb435a77f4d0fa89e273a86c4eea4f482
Ajay Gangwar <ajaygangwar24598@gmail.com>
2025-01-14 12:27:57 +0530

    added practice program

---
+++ lecture2/variables.py
@@ -0,0 +1,11 @@
+a = 28          # int
+b = 1.5         # float
+c = "hello"     # str
+d = True        # bool
+e = None        # NoneType
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(e, type(e))
