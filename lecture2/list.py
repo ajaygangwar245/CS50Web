a7293dd59b85a187005734d53fbb4f1b18a5fea0
Ajay Gangwar <ajaygangwar24598@gmail.com>
2025-01-14 12:32:04 +0530

    added practice program

---
+++ lecture2/list.py
@@ -0,0 +1,20 @@
+# list of names
+names = ["Harry", "Ron", "Ginny", "Hermione", "Neville"]
+
+# print 
+print(names)
+
+# add new element to the list
+names.append("Draco")
+
+# print
+print(names)
+
+# remove element from the list
+names.remove("Hermione")
+
+# sort the list
+names.sort()
+
+# print
+print(names)
