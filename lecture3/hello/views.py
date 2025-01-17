2291c84d6e395e1f68f2379942440ddc0d3d0214
Ajay Gangwar <ajaygangwar24598@gmail.com>
2025-01-18 02:49:05 +0530

    updated all files

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,10 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    # return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
 
 def ajay(request):
     return HttpResponse("Hello, Ajay!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    # return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
