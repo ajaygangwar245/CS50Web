c77d7e25ce3e117181357366c5e633fa6f69043f
Ajay Gangwar <ajaygangwar24598@gmail.com>
2025-01-20 07:51:51 +0530

    updated urls file

--- lecture3/lecture3/urls.py
+++ lecture3/lecture3/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
-    path('republic/', include("republic.urls"))
+    path('republic/', include("republic.urls")),
+    path('tasks/', include("tasks.urls"))
 ]
